cae03dbfa6c30a1cae4ecafd1b6490e387fc21c2
Ruijuan Li <ruijuanli@whitney.(none)>
2017-04-24 22:32:12 -0700

    run map construction for each chromosome seperately

--- map_A01.R
+++ map_A01.R
@@ -5,9 +5,10 @@ library(ggplot2)
 library(reshape)
 library(dplyr)
 
+F2.data <- read.mapmaker(file="~/F2/output/F2_geno_for_one_map_final.txt")
 load("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/twopts.f2.04.21.Rdata")
 
-mark.f2.A01 <- make.seq(twopts.f2.04.21, 1:1095)
+mark.f2.A01 <- make.seq(twopts.f2.04.24, 1:403)
 LG1.f2.ord.A01 <- order.seq(input.seq = mark.f2.A01, n.init = 5,
                         subset.search = "twopt",
                         twopt.alg = "rcd", THRES = 6,
