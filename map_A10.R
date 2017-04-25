070b2e0b9c3c05f29731528094a293a2452f35b8
Ruijuan Li <ruijuanli@whitney.(none)>
2017-04-24 22:32:12 -0700

    run map construction for each chromosome seperately

--- map_A10.R
+++ map_A10.R
@@ -8,15 +8,15 @@ library(dplyr)
 F2.data <- read.mapmaker(file="~/F2/output/F2_geno_for_one_map_final.txt")
 load("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/twopts.f2.04.21.Rdata")
 
-mark.f2.A01 <- make.seq(twopts.f2.04.24, 1:403)
-LG1.f2.ord.A01 <- order.seq(input.seq = mark.f2.A01, n.init = 5,
+mark.f2.A10 <- make.seq(twopts.f2.04.24, 3393:3759)
+LG1.f2.ord.A10 <- order.seq(input.seq = mark.f2.A10, n.init = 5,
                         subset.search = "twopt",
                         twopt.alg = "rcd", THRES = 6,
                         draw.try = T, wait = 1,
                         touchdown = T) 
 
 
-save(LG1.f2.ord.A01, file = "/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/LG1.f2.ord.A01.Rdata")
+save(LG1.f2.ord.A10, file = "/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/LG1.f2.ord.A10.Rdata")
 
 
 
--- map_C04.R
+++ map_C04.R
@@ -8,15 +8,15 @@ library(dplyr)
 F2.data <- read.mapmaker(file="~/F2/output/F2_geno_for_one_map_final.txt")
 load("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/twopts.f2.04.21.Rdata")
 
-mark.f2.A01 <- make.seq(twopts.f2.04.24, 1:403)
-LG1.f2.ord.A01 <- order.seq(input.seq = mark.f2.A01, n.init = 5,
+mark.f2.C04 <- make.seq(twopts.f2.04.24, 4668:4869)
+LG1.f2.ord.C04 <- order.seq(input.seq = mark.f2.C04, n.init = 5,
                         subset.search = "twopt",
                         twopt.alg = "rcd", THRES = 6,
                         draw.try = T, wait = 1,
                         touchdown = T) 
 
 
-save(LG1.f2.ord.A01, file = "/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/LG1.f2.ord.A01.Rdata")
+save(LG1.f2.ord.C04, file = "/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/LG1.f2.ord.C04.Rdata")
 
 
 
