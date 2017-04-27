2e7e3f0c074d1419c65c6de3264be48834358ac9
Ruijuan Li <ruijuanli@whitney.(none)>
2017-04-27 14:10:57 -0700

    modified 505 GWAS code

--- GWAS_505_leaf.R
+++ GWAS_505_leaf.R
@@ -1,15 +1,15 @@
 # load library
-source("http://www.bioconductor.org/biocLite.R")
-biocLite("multtest")
-install.packages("gplots")
-install.packages("LDheatmap")
-install.packages("genetics")
-install.packages("EMMREML") 
-install.packages("scatterplot3d")
+# source("http://www.bioconductor.org/biocLite.R")
+# biocLite("multtest")
+# install.packages("gplots")
+# install.packages("LDheatmap")
+# install.packages("genetics")
+# install.packages("EMMREML") 
+# install.packages("scatterplot3d")
 
 library(multtest)
 library(gplots)
-library(LDheatmap) # this could not be succesfully installed 
+# library(LDheatmap) # this could not be succesfully installed 
 library(genetics)
 library(EMMREML)
 library(compiler) #this library is already installed in R
@@ -20,66 +20,51 @@ source("http://zzlab.net/GAPIT/gapit_functions.txt")
 # intall EMMA package 
 source("http://zzlab.net/GAPIT/emma.txt")
 
+# set working directory 
+setwd("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output/myGAPIT/")
+
 ######### load data
 # genotype data 
-setwd("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output")
-load("~/505/output/leaf_505.Rdata")
-
-# reform the data
-temp <- leaf_505 
-
-  vcfbi <- is.biallelic(temp) # return with logics indicating whehter biallelic or not... 
-  vcfref <- subset(getREF(temp), subset = vcfbi) # get ref allele
-  vcfalt <- subset(getALT(temp), subset = vcfbi) # get alt allele
-  vcfchrom <- subset(getCHROM(temp), subset = vcfbi) # get chromosome info 
-  vcfpos <- subset(getPOS(temp), subset = vcfbi) # get pos info 
-  vcfgts <- subset(extract.gt(temp, element = "GT", IDtoRowNames = F), subset = vcfbi) 
-
-  temp2 <- data.frame(cbind(vcfchrom,vcfpos,vcfref,vcfalt,vcfgts))
-  colnames(temp2)[1:4] <- c("CHROM","POS","REF","ALT")
-  rnames <- rownames(temp2) 
-  temp2 <- data.frame(sapply(temp2, function(x) sub("0/0","0",x)))
-  temp2 <- data.frame(sapply(temp2, function(x) sub("0/1","1",x)))
-  temp2 <- data.frame(sapply(temp2, function(x) sub("1/1","2",x)))
-  row.names(temp2) <- rnames
-  save(temp2, file="/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output/temp2.Rdata")
-  write.table(temp2, file= "/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output/temp2.txt")
-  rownames(temp2) <- paste(temp2$CHROM, temp2$POS, sep="_")
-
-# GD file 
-leaf_505.2 <- temp2[, 5:98] 
-leaf_505.2.t <- t(leaf_505.2)
-save(leaf_505.2.t, file = "leaf_505.2.Rdata")
-write.table(leaf_505.2.t, file = "leaf_505.2.txt")
-
-# leaf_505.2.matrix <- data.matrix(leaf_505.2.t)
-# GM file 
- 
-SNP.info <- data.frame(matrix(nrow = ncol(leaf_505.2.t), ncol = 3))
-colnames(SNP.info) <- c("Name", "Chromosome", "Position")
-SNP.info$Name <- colnames(leaf_505.2.t)
-SNP.info$Chromosome <- gsub("([[:print:]]+)(_)([[:digit:]]+)", "\\1", colnames(leaf_505.2.t))
-SNP.info$Position <- gsub("([[:print:]]+)(_)([[:digit:]]+)", "\\3", colnames(leaf_505.2.t))
+geno_505_hmp <- read.table("~/505/vcf_leaf_no_pop/combined/505_filtered_sorted.hmp.txt", head=FALSE)
+# get chrom name to numerics
+geno_505_hmp$V3 <- as.numeric(geno_505_hmp$V3)
+geno_505_hmp[1,3] <- "chrom"
 
 # phenotype data 
-load("")
+load("~/505/data/phenotype/gh.seed.bolt.table.wide.Rdata")
+# reform phentype data to have match taxa ID with geno data
+# get taxa ID from sample description file 
+sample_des_a_revised <- read.csv("~/505/data/phenotype/batch_a_revised.csv", header=T)
+sample_des_b_revised <- read.csv("~/505/data/phenotype/batch_b_revised.csv", header=T)
 
+sample_des_a_sub2 <- sample_des_a_revised[,c("Sample.ID", "No..of.Sowing", "Name")]
+sample_des_a_sub2 <- sample_des_a_sub2[!is.na(sample_des_a_sub2$Sample.ID),]
+sample_des_b_sub2 <- sample_des_b_revised[,c("Sample.ID", "No..of.Sowing", "Name")]
+sample_des_sub2 <- rbind(sample_des_a_sub2, sample_des_b_sub2)
 
+# add name to to replace SN 
+sample_des_sub2$SN <- paste("K", sample_des_sub2$No..of.Sowing, sep="")
+gh.seed.bolt.table.wide.merge <- merge(sample_des_sub2, gh.seed.bolt.table.wide, by = "SN")
+pheno_data_505 <- gh.seed.bolt.table.wide.merge[,c("Sample.ID", "FATTY ACID_Erucic acid (C22:1n9)", "FATTY ACID_oil contents (%)", "FATTY ACID_Oleic acid (C18:1n9c)")]
+colnames(pheno_data_505) <- c("Taxa", "Erucic_acid", "Oil_content", "Oleic_acid")
+pheno_data_505
 
+myY <- pheno_data_505
+myG <- geno_505_hmp
 
+# subset to get only 1st chromosome result 
+# myG.01 <- myG[myG$V3==1,]
+# myG.01.final <- data.frame(rbind(myG[1,], myG.01)) 
 
+# run GAPIT 
+myGAPIT <- GAPIT( 
+Y=myY,
+G=myG, 
+PCA.total=3,
+Geno.View.output=FALSE, 
+PCA.View.output=FALSE 
+)  
 
-
-
-
-
-
-
-
-
-
-
-
-
+print("finished, YAAH!")
 
 
