9dde1d1c59b0c63f9e140d71dedac1e67c9ec2f7
Ruijuan Li <ruijuanli@whitney.(none)>
2017-05-11 09:18:22 -0700

    update

--- subset_by_chromosome.sh
+++ subset_by_chromosome.sh
@@ -4,7 +4,7 @@ chrom=`cat chrom_list_napus_plus_Bsub`
 for i in $chrom 
 	do
 	echo $i
-	grep "$i" 2_ABC_depth > 2_ABC_depth_${i} 
-	grep "$i" 2_AC_depth > 2_AC_depth_${i}
+	grep "$i" 2_Unique.sorted_ABC.bam_depth > 2_ABC_depth_${i} 
+	grep "$i" 2_Unique.sorted_AC.bam_depth > 2_AC_depth_${i}
 
 done 
