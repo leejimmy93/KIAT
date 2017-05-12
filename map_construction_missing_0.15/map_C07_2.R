c4deb7b2c7a244089873be4ab964ee63ae902fef
Ruijuan Li <ruijuanli@whitney.(none)>
2017-05-12 13:51:56 -0700

    re-map using data with missing data filtered 0.15

---
+++ map_construction_missing_0.15/map_C07_2.R
@@ -0,0 +1,23 @@
+library(tcltk)
+library(tkrplot)
+library(onemap)  
+library(ggplot2)
+library(reshape)
+library(dplyr)
+
+F2.data <- read.mapmaker(file="~/F2/output/missing_rate_0.15/F2_geno_for_one_map_final.txt")
+load("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/missing_rate_0.15/twopts.f2.LOD3_rf0.5.Rdata")
+
+mark.f2.A01 <- make.seq(twopts.f2.LOD3_rf0.5, 2729:2828)
+LG.f2.ord.C07 <- order.seq(input.seq = mark.f2.C07, n.init = 5,
+                        subset.search = "twopt",
+                        twopt.alg = "rcd", THRES = 6,
+                        draw.try = T, wait = 1,
+                        touchdown = T) 
+
+
+save(LG.f2.ord.C07, file = "/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/F2/output/missing_rate_0.15/LG.f2.ord.C07.Rdata")
+
+
+
+
