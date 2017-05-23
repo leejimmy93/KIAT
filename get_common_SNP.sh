f997203eca8e75484241686b58407809583f5ca1
Ruijuan Li <ruijuanli@whitney.(none)>
2017-05-23 14:42:14 -0700

    update Da-Ae heterzygosity check

--- get_common_SNP.sh
+++ get_common_SNP.sh
@@ -7,18 +7,26 @@
 #done
 
 # get common position 
-sort 6_filtered.recode.vcf_pos Ae_Gae_2_filtered.recode.vcf_pos | uniq -d > 6_Ae_Gae_2
-sort Ae_combined_filtered.recode.vcf_pos Ae_Gae_3_filtered.recode.vcf_pos | uniq -d > combined_Ae_Gae_3
+# sort 6_filtered.recode.vcf_pos Ae_Gae_2_filtered.recode.vcf_pos | uniq -d > 6_Ae_Gae_2
+# sort Ae_combined_filtered.recode.vcf_pos Ae_Gae_3_filtered.recode.vcf_pos | uniq -d > combined_Ae_Gae_3
 
-sort 6_Ae_Gae_2 combined_Ae_Gae_3 | uniq -d > common_POS
+# sort 6_Ae_Gae_2 combined_Ae_Gae_3 | uniq -d > common_POS
 
 # filter based on position 
+# sample=`cat sample_list`
+# echp $sample
+# for i in $sample
+#	do
+#	vcftools --positions common_POS --vcf ${i}_filtered.recode.vcf --recode --recode-INFO-all --out ${i}_common.vcf
+# done
+
+# index 
 sample=`cat sample_list`
-echp $sample
+echo $sample
 for i in $sample
 	do
-	vcftools --positions common_POS --vcf ${i}_filtered.recode.vcf --recode --recode-INFO-all --out ${i}_common.vcf
+	bgzip -c ${i}_common.vcf.recode.vcf > ${i}_commin.vcf.gz
+	tabix -p vcf ${i}_commin.vcf.gz  
 done
-
 
 
