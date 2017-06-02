173c09a7dbeb33470b4b1f45d21ad6d729c21fc3
Ruijuan Li <ruijuanli@whitney.(none)>
2017-06-02 14:09:00 -0700

    505 & Da-Ae heterzygosity check

--- filter_Ae_flowering.sh
+++ filter_Ae_flowering.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Da-Ae_Da_Ol-1_heterzygosity_check
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Da-Ae_Da_Ol-1_heterzygosity_check/single_end_mapping
 
 sample=`cat sample_list`
 echo $sample
@@ -7,7 +7,7 @@ echo $sample
 for i in $sample
 	do
 
-vcftools --gzvcf ${i}_paired.star.trim.dir.vcf --remove-indels --min-alleles 2 --max-alleles 2 --minQ 40 --min-meanDP 20 --max-meanDP 500 --minGQ 30 --recode --recode-INFO-all --out ${i}_filtered 
+vcftools --gzvcf ${i}.vcf --remove-indels --min-alleles 2 --max-alleles 2 --minQ 40 --min-meanDP 20 --max-meanDP 500 --minGQ 30 --recode --recode-INFO-all --out ${i}_filtered 
 
 
 done
