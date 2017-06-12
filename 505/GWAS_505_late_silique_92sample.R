e728113272eb1368bbc409bd5b38b3c19cdd1ed3
Ruijuan Li <ruijuanli@whitney.(none)>
2017-06-12 14:30:28 -0700

    GWAS for common lines between leaf & late silique

--- 505/GWAS_505_late_silique_92sample.R
+++ 505/GWAS_505_late_silique_92sample.R
@@ -21,11 +21,11 @@ source("http://zzlab.net/GAPIT/gapit_functions.txt")
 source("http://zzlab.net/GAPIT/emma.txt")
 
 # set working directory 
-setwd("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output/myGAPIT/leaf_92")
+setwd("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output/myGAPIT/late_silique_92")
 
 ######### load data
 # genotype data 
-geno_505_hmp <- read.table("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/leaf_VS_late_silique/505_filtered_leaf_92_sorted.hmp.txt", head=FALSE)
+geno_505_hmp <- read.table("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/leaf_VS_late_silique/505_filtered_late_silique_92_sorted.hmp.txt", head=FALSE)
 # get chrom name to numerics
 geno_505_hmp$V3 <- as.numeric(geno_505_hmp$V3)
 geno_505_hmp[1,3] <- "chrom"
@@ -34,24 +34,32 @@ geno_505_hmp[1,3] <- "chrom"
 load("~/505/data/phenotype/gh.seed.bolt.table.wide.Rdata")
 # reform phentype data to have match taxa ID with geno data
 # get taxa ID from sample description file 
-sample_des_a_revised <- read.csv("~/505/data/phenotype/batch_a_revised.csv", header=T)
-sample_des_b_revised <- read.csv("~/505/data/phenotype/batch_b_revised.csv", header=T)
+sample_des_c <- read.csv("~/505/data/phenotype/batch_c.csv", header=T)
+sample_des_d <- read.csv("~/505/data/phenotype/batch_d.csv", header=T)
+sample_des_e <- read.csv("~/505/data/phenotype/batch_e.csv", header=T)
 
-sample_des_a_sub2 <- sample_des_a_revised[,c("Sample.ID", "No..of.Sowing", "Name")]
-sample_des_a_sub2 <- sample_des_a_sub2[!is.na(sample_des_a_sub2$Sample.ID),]
-sample_des_b_sub2 <- sample_des_b_revised[,c("Sample.ID", "No..of.Sowing", "Name")]
-sample_des_sub2 <- rbind(sample_des_a_sub2, sample_des_b_sub2)
+sample_des_c_sub2 <- sample_des_c[,c("Sample.ID", "No..of.Sowing", "Name")]
+sample_des_d_sub2 <- sample_des_d[,c("Sample.ID", "No..of.Sowing", "Name")]
+sample_des_e_sub2 <- sample_des_e[,c("Sample.ID", "No..of.Sowing", "Name")]
 
-# add name to to replace SN 
+sample_des_d_sub2 <- sample_des_d_sub2[c(1:41),]
+sample_des_sub2 <- rbind(sample_des_c_sub2, sample_des_d_sub2, sample_des_e_sub2)
+
+# add name to to replace SN
 sample_des_sub2$SN <- paste("K", sample_des_sub2$No..of.Sowing, sep="")
 gh.seed.bolt.table.wide.merge <- merge(sample_des_sub2, gh.seed.bolt.table.wide, by = "SN")
 pheno_data_505 <- gh.seed.bolt.table.wide.merge[,c("Sample.ID", "FATTY ACID_Erucic acid (C22:1n9)", "FATTY ACID_oil contents (%)", "FATTY ACID_Oleic acid (C18:1n9c)")]
 colnames(pheno_data_505) <- c("Taxa", "Erucic_acid", "Oil_content", "Oleic_acid")
 pheno_data_505
+K250_K251 <- data.frame(Taxa = c("505_K_250", "505_K_251"),
+			Erucic_acid = rep(NA, 2),
+			Oil_content = rep(NA, 2),
+			Oleic_acid = rep(NA, 2))
+pheno_data_505 <- rbind(pheno_data_505, K250_K251)
 
 # get the 92 common lines between leaf & late silique
 load("~/505/output/common.lines.leaf.131.sample.Rdata")
-pheno_data_505_92 <- pheno_data_505[which(pheno_data_505$Taxa %in% common.lines$Taxa.x),]
+pheno_data_505_92 <- pheno_data_505[which(pheno_data_505$Taxa %in% common.lines$Taxa.y),]
 rownames(pheno_data_505_92) <- 1:nrow(pheno_data_505_92)
 
 myY <- pheno_data_505_92
