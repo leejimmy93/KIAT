cfd5c3de5352a1ff767192f707f5f964074b1e23
Ruijuan Li <ruijuanli@whitney.(none)>
2017-06-13 11:10:15 -0700

    for 505 GWAS result check

--- function_BnRNAseq.R
+++ function_BnRNAseq.R
@@ -600,8 +600,35 @@ get.vcf <- function(test.2, vcf){
   return(vcf.new) 
 }
  
+#### plot manhattan result 
+library("qqman")
+plot_manhattan_oil <- function(sample_ID){
 
+  Erucic_acid <- read.csv("~/505/output/myGAPIT/sample_ID/GAPIT..Erucic_acid.GWAS.Results.csv", header = T)
 
+  Oil_content <- read.csv("~/505/output/myGAPIT/sample_ID/GAPIT..Oil_content.GWAS.Results.csv", header = T)
+
+  Oleic_acid <- read.csv("~/505/output/myGAPIT/sample_ID/GAPIT..Oleic_acid.GWAS.Results.csv", header = T)
+
+  colnames(Erucic_acid)[1:4] <- c("SNP", "CHR", "BP", "P")
+  colnames(Oil_content)[1:4] <- c("SNP", "CHR", "BP", "P")
+  colnames(Oleic_acid)[1:4] <- c("SNP", "CHR", "BP", "P")
+
+  png("~/505/output/figure/sample_ID/Erucic_acid.png", width=12, height=10, units="in", res=300)
+  par(mfrow=c(3,1)) 
+  manhattan(Erucic_acid, main = "Erucic acid", ylim = c(0, 8), cex = 0.6, 
+            cex.axis = 0.9, col = c("blue4", "orange3"), supggestiveline = 5, genomewideline = F 
+  ) 
+
+  manhattan(Oleic_acid, main = "Oleic acid", ylim = c(0, 8), cex = 0.6, 
+            cex.axis = 0.9, col = c("blue4", "orange3"), suggestiveline = 5, genomewideline = F 
+  ) 
+
+  manhattan(Oil_content, main = "Oil content", ylim = c(0, 8), cex = 0.6, 
+            cex.axis = 0.9, col = c("blue4", "orange3"), suggestiveline = 5, genomewideline = F
+  ) 
+  dev.off()  
+}
 
 
 
