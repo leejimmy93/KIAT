00ef16f7df7b6d7343a743a25074f063fd52af70
Ruijuan Li <ruijuanli@whitney.(none)>
2017-06-21 12:06:33 -0700

    505 no significant SNP troubleshooting update

---
+++ 505/GWAS_505_sig_leaf_other_silique.R
@@ -0,0 +1,52 @@
+# load library
+# source("http://www.bioconductor.org/biocLite.R")
+# biocLite("multtest")
+# install.packages("gplots")
+# install.packages("LDheatmap")
+# install.packages("genetics")
+# install.packages("EMMREML") 
+# install.packages("scatterplot3d")
+
+library(multtest)
+library(gplots)
+# library(LDheatmap) # this could not be succesfully installed 
+library(genetics)
+library(EMMREML)
+library(compiler) #this library is already installed in R
+library("scatterplot3d")
+
+# install GAPIT package 
+source("http://zzlab.net/GAPIT/gapit_functions.txt")
+# intall EMMA package 
+source("http://zzlab.net/GAPIT/emma.txt")
+
+# set working directory 
+setwd("/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/output/myGAPIT/sig_leaf_other_silique")
+
+######### load data
+# genotype data 
+geno_505_hmp <- read.table("~/505/leaf_VS_late_silique/sig_leaf_other_silique.hmp.txt", head=FALSE)
+# get chrom name to numerics
+geno_505_hmp$V3 <- as.numeric(geno_505_hmp$V3)
+geno_505_hmp[1,3] <- "chrom"
+
+# phenotype data 
+load("~/505/leaf_VS_late_silique/common_92_pheno.Rdata") 
+pheno_data_505 <- common_92_pheno  
+
+myY <- pheno_data_505
+myG <- geno_505_hmp
+
+# run GAPIT 
+myGAPIT <- GAPIT( 
+Y=myY,
+G=myG, 
+PCA.total=0,
+Geno.View.output=FALSE, 
+PCA.View.output=FALSE,
+Model.selection = TRUE 
+)  
+
+print("finished, YAAH!")
+
+
