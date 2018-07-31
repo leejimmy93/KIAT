c2ed5f1db206d4fe84f94211fb64a41417ee82b2
Ruijuan Li <rzl0007@auburn.edu>
2018-07-30 18:36:38 -0700

    get uniquely mappred reads

---
+++ parent/extract_unique_mapping_reads.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/B_subgenome_check/output/mapping_result/AC
+
+sample=`cat sample_list`
+echo $sample
+
+for i in $sample 
+	do
+	echo ${i} 
+	samtools view ${i}Aligned.sortedByCoord.out.bam | awk '$5 == "255"' > ${i}_Unique.sorted.sam
+	samtools view -bT /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/Reference/B.napus/Brassica_napus_v4.1.chromosomes.fa ${i}_Unique.sorted.sam > ${i}_Unique.sorted.bam
+
+	rm *.sam 
+done 
+
