5f407555a775c984188e03757a02296524b56485
Ruijuan Li <rzl0007@auburn.edu>
2018-10-10 15:00:51 -0700

    update

--- 505/WGS/filter.sh
+++ 505/WGS/filter.sh
@@ -7,7 +7,7 @@ outdir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanl
 for i in $chrom
 	do 
 	echo $i
-	vcftools --gzvcf ${i}.vcf.gz --remove-indels --min-alleles 2 --max-alleles 2 --maf 0.05 --minQ 40 --minGQ 30 --min-meanDP 5 --max-meanDP 500 --max-missing 0.2 --recode --recode-INFO-all --out ${outdir}/${i}_filtered
+	vcftools --gzvcf ${i}.vcf.gz --remove-indels --min-alleles 2 --max-alleles 2 --maf 0.05 --minQ 40 --minGQ 30 --min-meanDP 0 --max-meanDP 500 --max-missing 0.2 --recode --recode-INFO-all --out ${outdir}/${i}_filtered
 	mv ${outdir}/${i}_filtered.recode.vcf ${outdir}/${i}_filtered.vcf 
 	gzip ${outdir}/${i}_filtered.vcf 
 done
