b4e2191cef681430cfeb4c0131e3fc33495e3d51
Ruijuan Li <rzl0007@auburn.edu>
2018-10-11 14:04:44 -0700

    update

--- 505/filter_late_silique.sh
+++ 505/filter_late_silique.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/vcf_late_silique_131_sample/
 
-chrom=`echo chrA10`
-outdir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/vcf_combine_WGS_RNAseq/test
+chrom=`ls *.gz  | sed 's/.vcf.gz//g'`
+outdir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/vcf_combine_WGS_RNAseq/RNA_filtered
 
 for i in $chrom
 	do 
 	echo $i
-	vcftools --gzvcf ${i}.vcf.gz --remove-indels --min-alleles 2 --max-alleles 2 --maf 0.05 --minQ 40 --minGQ 30 --min-meanDP 5 --max-meanDP 500 --max-missing 0.2 --recode --recode-INFO-all --out ${outdir}/${i}_filtered
+	vcftools --gzvcf ${i}.vcf.gz --remove-indels --min-alleles 2 --max-alleles 2 --maf 0.05 --minQ 40 --minGQ 30 --min-meanDP 5 --max-meanDP 500 --max-missing 0.5 --recode --recode-INFO-all --out ${outdir}/${i}_filtered
 	mv ${outdir}/${i}_filtered.recode.vcf ${outdir}/${i}_filtered.vcf 
 	gzip ${outdir}/${i}_filtered.vcf 
 done
