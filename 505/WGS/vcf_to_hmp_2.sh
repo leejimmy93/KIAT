d360b5c9e4f80c631fa867a44136fa057b0da196
Ruijuan Li <rzl0007@auburn.edu>
2018-10-21 18:14:19 -0700

    why RNA SNPs are not called from WGS samples

--- 505/WGS/vcf_to_hmp_2.sh
+++ 505/WGS/vcf_to_hmp_2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/vcf_combine_WGS_RNAseq
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/WGS_RNA
 
 # WGS 
-cd WGS_filtered_2
+# cd WGS_filtered_2
 
-file=`ls *.gz | sed 's/_filtered.vcf.gz//g'`
+file=`ls *.gz | sed 's/_population_all.vcf.gz//g'`
 for i in $file	
 	do
 	echo $i
-	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -SortGenotypeFilePlugin -inputFile ${i}_filtered.vcf.gz -outputFile ${i}_filtered -fileType VCF
-	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -fork1 -vcf ${i}_filtered.vcf -export -exportType Hapmap -runfork1
+	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -SortGenotypeFilePlugin -inputFile ${i}_population_all.vcf.gz -outputFile ${i} -fileType VCF
+	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -fork1 -vcf ${i}.vcf -export -exportType Hapmap -runfork1
 done
 
