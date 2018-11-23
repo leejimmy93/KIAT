c49b530a5b01046a9c572ad075046b855b9b1446
Ruijuan Li <rzl0007@auburn.edu>
2018-11-22 16:25:21 -0800

    update

--- 505/WGS/filter.sh
+++ 505/WGS/filter.sh
@@ -7,7 +7,7 @@ outdir=/Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanl
 for i in $chrom
 	do 
 	echo $i
-	vcftools --gzvcf ${i}.vcf.gz --remove-indels --min-alleles 2 --max-alleles 2 --maf 0.05 --minQ 40 --minGQ 30 --min-meanDP 1 --max-meanDP 100 --max-missing 0.5 --recode --recode-INFO-all --out ${outdir}/${i}_filtered
+	vcftools --gzvcf ${i}.vcf.gz --remove-indels --min-alleles 2 --max-alleles 2 --maf 0.01 --minQ 40 --minGQ 30 --min-meanDP 1 --max-meanDP 100 --max-missing 0.5 --recode --recode-INFO-all --out ${outdir}/${i}_filtered
 	mv ${outdir}/${i}_filtered.recode.vcf ${outdir}/${i}_filtered.vcf 
 	gzip ${outdir}/${i}_filtered.vcf 
 done
---
+++ 505/calc.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+
+cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/505/vcf_late_silique_131_sample/combined
+
+file_in=$1
+prefix=$2
+
+vcftools --gzvcf $file_in \
+	 --out $prefix \
+         --freq  
+echo "done frequency calculation"
+
+#vcftools --gzvcf $file_in \
+#         --out $prefix \
+#         --depth
+# echo "done depth calculation"
+
+# vcftools --gzvcf $file_in \
+#         --out $prefix \
+#         --site-depth
+# echo "done site depth"
+
+# vcftools --gzvcf $file_in \
+#        --out $prefix \
+#        --site-mean-depth
+# echo "done site mean depth"
+
+# vcftools --gzvcf $file_in \
+#         --out $prefix \
+#         --counts
+# echo "done counts"
+
+# vcftools --gzvcf $file_in \
+#         --out $prefix \
+#         --site-pi
+# echo "done site-pi"
+
+# vcftools --gzvcf $file_in \
+#         --out $prefix \
+#         --het
+# echo "done het"
+
+vcftools --gzvcf $file_in \
+         --out $prefix \
+         --site-quality
+echo "done site quality"
+
+vcftools --gzvcf $file_in \
+         --out $prefix \
+         --missing-indv
+echo "done missing indv"
+
+vcftools --gzvcf $file_in \
+         --out $prefix \
+         --missing-site
+echo "done missing site"
+
