48dac6f1a2c53358b5fc8fa64d1970f9b56c305a
Ruijuan Li <rzl0007@auburn.edu>
2019-02-03 12:34:44 -0800

    update 505 GWAS

--- 505/WGS/vcf_to_hmp_2.sh
+++ 505/WGS/vcf_to_hmp_2.sh
@@ -5,11 +5,11 @@ cd /Network/Servers/avalanche.plb.ucdavis.edu/Volumes/Mammoth/Users/ruijuanli/50
 # WGS 
 # cd WGS_filtered_2
 
-file=`ls *.vcf.gz | sed 's/_filtered.vcf.gz//g'`
+file=`echo 505_filtered_het_0.2.recode.vcf.gz | sed 's/.recode.vcf.gz//g'`
 for i in $file	
 	do
 	echo $i
-	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -SortGenotypeFilePlugin -inputFile ${i}_filtered.vcf.gz -outputFile ${i} -fileType VCF
+	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -SortGenotypeFilePlugin -inputFile ${i}.recode.vcf.gz -outputFile ${i} -fileType VCF
 	/usr/local/stow/tassel-5-standalone/run_pipeline.pl -Xmx50g -fork1 -vcf ${i}.vcf -export -exportType Hapmap -runfork1
 done
 
